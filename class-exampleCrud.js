a0b5c8f2e9d712444907587c939dfe965cee7ccf
SHOTA GUTERRES <SHOTA@Peripatetica.local>
2016-06-29 21:08:29 -0700

    starting to run out of steam. might drink coffee tonight

---
+++ class-exampleCrud.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  // create object instance of my Firebase database
+  var myDBReference = new Firebase('https://fan-message-board.firebaseio.com/');
+
+  var sourceTemplate = $('#list-template').html();
+  var template = Handlebars.compile(sourceTemplate);
+
+  // define submit event listener/handler
+  $('#message-form').submit(function(event) {
+    // prevents page refresh
+    event.preventDefault();
+
+    // grab user input
+    var message = $('#message').val();
+    var messagesReference = myDBReference.child('messages');
+    messagesReference.push({
+      message: message
+    });
+  });
+
+  // Read functionality
+  myDBReference.child('messages').on('child_added', function(results) {
+    results.forEach(function(message) {
+
+      var data = {
+        message: message.val(),
+        id: results.key()
+      };
+
+      var templateHTML = template(data);
+
+      var $templateHTML = $(templateHTML);
+
+      $templateHTML.click(function() {
+        var messageId = $(this).data('id');
+        updateMessage(messageId);
+      })
+      $('#messages-list').append($templateHTML);
+    });
+  });
+
+  // Update Functionality
+  function updateMessage(id, message) {
+    var messageReference = new Firebase('https://fan-message-board.firebaseio.com/messages/' + id);
+
+    messageReference.update({
+      message: 'fu man shu'
+    });
+
+  }
+
+  // Delete functionality
+  function deleteMessage(id) {
+    var messageReference = new Firebase('https://fan-message-board.firebaseio.com/messages/' + id);
+
+    messageReference.remove();
+  }
+
+});
---
+++ crud.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+  var myDBReference = new Firebase('https://smapfaminfo.firebaseio.com//');
+  
+  $('.check').on("click", function(event) {
+    event.preventDefault();
+
+    var parentInfo = new Object();
+      parentInfo.momFirst = $('#mother-first').val();
+      parentInfo.momLast = $('#mother-last').val();
+      parentInfo.dadFirst = $('#father-first').val();
+      parentInfo.dadLast = $('#father-last').val();
+
+    var $momFirst = parentInfo.momFirst;
+    var $momLast = parentInfo.momLast;
+    var $dadFirst = parentInfo.dadFirst;
+    var $dadLast = parentInfo.dadLast;
+    // create an empty object, put the values of input field into that object
+    // push that data object to firebase
+
+    var familyInfo = myDBReference.child('parentInformation');  
+  
+    familyInfo.push({
+      momFirst: $momFirst,
+      momLast: $momLast,
+      dadFirst: $dadFirst,
+      dadLast: $dadLast   
+    });
+
+myDBReference.child('parentInformation').on('child_added', function(results) {
+    results.forEach(function(parentInformation) {
+      var data = {
+        momFirst: $('#mother-first').val(),
+        momLast: $('#mother-last').val(),
+        dadFirst: $('#father-first').val(),
+        dadLast: $('#father-last').val(), 
+        id: results.key()
+      }
+
+      var userEntryValue = parentInformation.A.B;
+      var userEntryKey = parentInformation.Y.path.u[2];
+      var dataKey = parentInformation.Y.path.u[1];
+      
+      var readableData = dataKey + " , " + userEntryValue + ": "+ userEntryKey;
+     console.log(readableData);
+     console.log(userEntryValue);
+      });
+
+      //console.log(parentInformation.A.B);
+      //console.log(parentInformation.Y.path.u[2]);
+      //console.log(parentInformation.Y.path.u[1]);
+      //console.log(parentInformation);
+      //console.log(parentInformation.Y.path.u.1);
+    });
+    
+    });
+
+  });
+
+  
+
+
+/*
+
+  // Update Functionality
+  function updateMessage(id, message) {
+    var messageReference = new Firebase('https://fan-message-board.firebaseio.com/messages/' + id);
+
+    messageReference.update({
+      message: 'fu man shu'
+    });
+
+  }
+
+  // Delete functionality
+  function deleteMessage(id) {
+    var messageReference = new Firebase('https://fan-message-board.firebaseio.com/messages/' + id);
+
+    messageReference.remove();
+  }
+
+*/
